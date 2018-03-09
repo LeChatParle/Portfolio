a9de62aa2ac12427b7fa1966b8ff2e04517495f2
Jeremy Edwards <edwardsbjeremy@gmail.com>
2018-03-09 15:27:08 -0600

    Added comments that include future project ideas

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+$(document).ready(function() {
+	$(".overlayDIV").hide();
+	$(".websitePicture").hover(
+		function() {
+  		$('.overlayDIV', this).fadeIn(200);
+  	}, 
+		function() {
+  		$('.overlayDIV', this).fadeOut(200);
+  	});
+});
